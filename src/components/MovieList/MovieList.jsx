eef16d2cb1e7e78c4f63e542efcc95e89e7467d3
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-03-01 14:43:58 +0100

    add adaptive designs

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,47 +1,48 @@
 import s from './MovieList.module.css';
-
+import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
 
 const MovieList = ({ dataMovies }) => {
-    if (dataMovies?.length === 0) {
-        return;
-    }
+	const location = useLocation();
 
-    const { results } = dataMovies;
-    const location = useLocation();
+	if (dataMovies?.length === 0) {
+		return;
+	}
 
-    return (
-        <>
-            <ul className={s.wrapper}>
-                {results?.map(movie => {
-                    return (
-                        <li key={movie.id} className={s.itemWrapper}>
-                            {
-                                <Link
-                                    to={`/movies/${movie.id.toString()}`}
-                                    state={location}
-                                >
-                                    <div className={s.contentWrapper}>
-                                        <div className={s.imgWrapper}>
-                                            <img
-                                                src={`https://image.tmdb.org/t/p/w500${
-                                                    movie.poster_path ??
-                                                    '/2IYrYumfQ6ikCWowxdRAUShg1Hm.jpg'
-                                                }`}
-                                                alt={movie.title}
-                                                width="220"
-                                            />
-                                        </div>
-                                        <p className={s.title}>{movie.title}</p>
-                                    </div>
-                                </Link>
-                            }
-                        </li>
-                    );
-                })}
-            </ul>
-        </>
-    );
+	const { results } = dataMovies;
+
+	return (
+		<>
+			<ul className={s.wrapper}>
+				{results?.map(movie => {
+					return (
+						<li key={movie.id} className={s.itemWrapper}>
+							{
+								<Link to={`/movies/${movie.id.toString()}`} state={location}>
+									<div className={s.contentWrapper}>
+										<div className={s.imgWrapper}>
+											<img
+												src={`https://image.tmdb.org/t/p/w500${
+													movie.poster_path ?? '/2IYrYumfQ6ikCWowxdRAUShg1Hm.jpg'
+												}`}
+												alt={movie.title}
+												width="220"
+											/>
+										</div>
+										<p className={s.title}>{movie.title}</p>
+									</div>
+								</Link>
+							}
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
+};
+
+MovieList.propTypes = {
+	dataMovies: PropTypes.array.isRequired,
 };
 
 export default MovieList;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,26 +4,26 @@ import clsx from 'clsx';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
-    const buildLinkClass = ({ isActive }) => {
-        return clsx(s.link, isActive && s.active);
-    };
+	const buildLinkClass = ({ isActive }) => {
+		return clsx(s.link, isActive && s.active);
+	};
 
-    return (
-        <div className={s.wrapper}>
-            <ul className={s.navlist}>
-                <li className={s.navlink}>
-                    <NavLink to={'/'} className={buildLinkClass}>
-                        Home
-                    </NavLink>
-                </li>
-                <li className={s.navlink}>
-                    <NavLink to={'/movies'} className={buildLinkClass}>
-                        Movies
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className={s.wrapper}>
+			<ul className={s.navlist}>
+				<li className={s.navlink}>
+					<NavLink to={'/'} className={buildLinkClass}>
+						Home
+					</NavLink>
+				</li>
+				<li className={s.navlink}>
+					<NavLink to={'/movies'} className={buildLinkClass}>
+						Movies
+					</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navigation;
